b52d18cd9d86e8cb6601f30f23fcd51354e33448
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-01-23 11:46:26 +0100

    draw boarder of the maze

--- draw_maze.py
+++ draw_maze.py
@@ -1,12 +1,27 @@
 import curses as cs
 
 
-def draw_waze():
+def draw_waze(maze_lines: list[str], config: dict):
     def draw(window: cs.window):
         cs.curs_set(0)
         window.clear()
 
-        window.addstr(0, 0, "=====")
+
+        width = config['WIDTH'] * 3
+        height = config['HEIGHT'] * 3
+
+        window.addch(0, 0, '┏')
+        window.addch(0, width, '┓')
+        window.addch(height, 0, '┗')
+        window.addch(height, width, '┛')
+
+        for c in range(1, width):
+            window.addch(0, c, '━')
+            window.addch(height, c, '━')
+
+        for r in range(1, height):
+            window.addch(r, 0, '┃')
+            window.addch(r, width, '┃')
 
         window.refresh()
         window.getch()
