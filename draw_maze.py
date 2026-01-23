76dea5be0f696bb3df593b0270f9c12054418b23
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-01-23 11:55:58 +0100

    draw boarder of the maze

--- draw_maze.py
+++ draw_maze.py
@@ -1,6 +1,22 @@
 import curses as cs
 
 
+def draw_board(window: cs.window, width: int, height: int):
+
+    window.addch(1, 0, '┏')
+    window.addch(0, width, '┓')
+    window.addch(height, 1, '┗')
+    window.addch(height, width, '┛')
+
+    for c in range(2, width):
+        window.addch(1, c, '━')
+        window.addch(height, c, '━')
+
+    for r in range(2, height):
+        window.addch(r, 1, '┃')
+        window.addch(r, width, '┃')
+
+
 def draw_waze(maze_lines: list[str], config: dict):
     def draw(window: cs.window):
         cs.curs_set(0)
@@ -9,18 +25,7 @@ def draw_waze(maze_lines: list[str], config: dict):
         width = config['WIDTH'] * 3
         height = config['HEIGHT'] * 3
 
-        window.addch(0, 0, '┏')
-        window.addch(0, width, '┓')
-        window.addch(height, 0, '┗')
-        window.addch(height, width, '┛')
-
-        for c in range(1, width):
-            window.addch(0, c, '━')
-            window.addch(height, c, '━')
-
-        for r in range(1, height):
-            window.addch(r, 0, '┃')
-            window.addch(r, width, '┃')
+        draw_board(window, width, height)
 
         window.refresh()
         window.getch()
